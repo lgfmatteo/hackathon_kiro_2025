3762abb206f29bd082115e1f95c499a9c289ce74
Pierre Bressollette <pbressollette@gmail.com>
2025-11-13 15:08:26 +0100

    glouton

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 
 # Choix de l'instance
-s = '01'
+s = '10'
 
 # Parsing des données
 vehicles_df = pd.read_csv('instances/vehicles.csv')
-instance_df = pd.read_csv('instances/instance_' + s + '.csv')
 
+instance_df = pd.read_csv('instances/instance_' + s + '.csv')
 depot = instance_df[instance_df['id'] == 0].iloc[0]
 orders = instance_df[instance_df['id'] != 0].sort_values('window_start').to_dict('records')
 
@@ -20,8 +20,8 @@ def glouton_simple(orders, vehicles_df):
         cheapest_vehicle = suitable_vehicles.sort_values('rental_cost').iloc[0]
         
         route = {
-            'family': cheapest_vehicle['family'],
-            'order_1': order['id']
+            'family': int(cheapest_vehicle['family']),
+            'order_1': int(order['id'])
         }
         routes.append(route)
     
